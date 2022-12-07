4443446490c02f5d4c3a54efb0517b4419e63f75
chemawall <jmmuro88@gmail.com>
2022-12-07 15:26:39 +0000

    tabla favorites dividida en dos

--- src/models.py
+++ src/models.py
@@ -8,7 +8,7 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class user(Base):
+class User(Base):
     __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
@@ -17,20 +17,27 @@ class user(Base):
     surname = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     date_suscription = Column(DateTime)
-    log = Column(Boolean), nullable=False)
+    log = Column(Boolean, nullable=False)
 
 
-class favorites(Base):
-    __tablename__ = 'favorites'
+class FavoritesPlanets(Base):
+    __tablename__ = 'favoritesplanets'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     planet_id= Column(Integer, ForeignKey('planet.id'))
+    user_id= Column(Integer, ForeignKey('user.id'))
+
+class FavoritesCharacter(Base):
+    __tablename__ = 'favoritescharacters'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
     character_id= Column(Integer, ForeignKey('character.id'))
     user_id= Column(Integer, ForeignKey('user.id'))
    
 
-class planet(Base):
+class Planet(Base):
     __tablename__ = 'planet'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
@@ -40,7 +47,7 @@ class planet(Base):
     climate = Column(String(250), nullable=False)
     orbit = Column(Integer, nullable=False)
    
-class character(Base):
+class Character(Base):
     __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
